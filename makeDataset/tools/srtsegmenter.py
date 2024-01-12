6c65044a2c58a589ac5a539f2c6b7cdab72af582
78Alpha <37676834+78Alpha@users.noreply.github.com>
2024-01-12 14:52:01 -0800

    Update srtsegmenter.py

--- makeDataset/tools/srtsegmenter.py
+++ makeDataset/tools/srtsegmenter.py
@@ -5,23 +5,32 @@ from phonemizer import phonemize
 import glob
 from tqdm import tqdm
 
-srt_list = glob.glob("./srt/*.srt")
-
-audio_list = glob.glob("./audio/*.wav")
-
 output_dir = './segmentedAudio/'   # path to where you want the segmented audio to go
 bad_audio_dir = './badAudio/'  # path to where you want the bad audio to go
+srt_directory = './srt/'
+audio_dir = './audio/'
 
-# os.makedirs(output_dir, exist_ok=True)
-# os.makedirs(bad_audio_dir, exist_ok=True)
+os.makedirs(output_dir, exist_ok=True)
+os.makedirs(bad_audio_dir, exist_ok=True)
+os.makedirs(srt_dir, exist_ok=True)
+os.makedirs(audio_dir, exists_ok=True)
+os.makedirs('./trainingdata', exists_ok=True)
 
-                   # path to /output.txt
-print(f"Processing: {len(srt_list)}")
-for sub_file in tqdm(srt_list):
+srt_list = glob.glob("./srt/*.srt")  # Get a list of all srt files
+
+audio_list = glob.glob("./audio/*.wav")  # Get a list of all audio files
+
+if len(srt_list) > 0 and len(audio_list) > 0:
+    pass
+else:
+    raise Exception(f"You need to have at least 1 srt file and 1 audio file, you have {len(srt_list)} srt and {len(audio_list)} audio files!")
+
+print(f"SRT Files: {len(srt_list)}")
+for sub_file in tqdm(srt_list):  # Iterate over all srt files
     subs = pysrt.open(sub_file)  # Path to .srt file
     audio_name = os.path.basename(sub_file).replace(".srt", ".wav")
 
-    audio = AudioSegment.from_wav(f'./audio/{audio_name}')  # path to .wav file
+    audio = AudioSegment.from_wav(f'./{audio_dir}/{audio_name}')  # path to .wav file
     with open('./trainingdata/output.txt', 'a+') as out_file:
         for i, sub in enumerate(subs):
 
@@ -38,7 +47,3 @@ for sub_file in tqdm(srt_list):
                 out_file.write(f'{filename}|{sub.text}|1\n')
             else:
                 audio_segment.export(os.path.join(bad_audio_dir, filename), format='wav')
-
-
-            #phonemized_text = phonemize(sub.text, language='en-us') # I was experimenting with phonemized .srt files. You can comment this out if you want
-            #phonemized_file.write(f'{i}\n{str(sub.start)} --> {str(sub.end)}\n{phonemized_text}\n\n')
