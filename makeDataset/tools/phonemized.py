a725be722da6660b49ae12e082fd55f60cc12c38
78Alpha <37676834+78Alpha@users.noreply.github.com>
2024-01-11 21:46:08 -0800

    Update phonemized.py
    Efficient phonemization

--- makeDataset/tools/phonemized.py
+++ makeDataset/tools/phonemized.py
@@ -1,15 +1,26 @@
 from phonemizer import phonemize
 import os
+from tqdm import tqdm
 
-with open('/home/eleven/StyleGuide/makeDataset/trainingdata/output.txt', 'r') as f: # Path to output.txt
+with open('./trainingdata/output.txt', 'r') as f: # Path to output.txt
     lines = f.readlines()
 
 # Phonemize the transcriptions
+phonemized = []
+filenames = []
+transcriptions = []
+speakers = []
 phonemized_lines = []
 for line in lines:
     filename, transcription, speaker = line.strip().split('|')
-    phonemes = phonemize(transcription, language='en-us', backend='espeak')
-    phonemized_lines.append((filename, f'{filename}|{phonemes}|{speaker}\n'))
+    filenames.append(filename)
+    transcriptions.append(transcription)
+    speakers.append(speaker)
+
+phonemized = phonemize(transcriptions, language="en-us", backend='espeak')
+
+for i in tqdm(range(len(filenames))):
+    phonemized_lines.append((filenames[i], f'{filenames[i]}|{phonemized[i]}|{speakers[i]}\n'))
 
 # Sort
 phonemized_lines.sort(key=lambda x: int(x[0].split('_')[1].split('.')[0]))
@@ -18,10 +29,10 @@ phonemized_lines.sort(key=lambda x: int(x[0].split('_')[1].split('.')[0]))
 train_lines = phonemized_lines[:int(len(phonemized_lines) * 0.9)]
 val_lines = phonemized_lines[int(len(phonemized_lines) * 0.9):]
 
-with open('/home/eleven/StyleGuide/makeDataset/trainingdata/train_list.txt', 'w') as f: # Path for train_list.txt in the training data folder
+with open('./trainingdata/train_list.txt', 'w+') as f: # Path for train_list.txt in the training data folder
     for _, line in train_lines:
         f.write(line)
 
-with open('/home/eleven/StyleGuide/makeDataset/trainingdata/val_list.txt', 'w') as f:  # Path for val_list.txt in the training data folder
+with open('./trainingdata/val_list.txt', 'w+') as f:  # Path for val_list.txt in the training data folder
     for _, line in val_lines:
         f.write(line)
