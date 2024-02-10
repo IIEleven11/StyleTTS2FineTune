c4a07dc0546f722147069a809691971f9f50b060
IIEleven11 <jakemottola6@gmail.com>
2024-02-10 14:45:49 -0800

    added padding script

---
+++ makeDataset/tools/add_padding.py
@@ -0,0 +1,22 @@
+from pydub import AudioSegment
+import os
+import glob
+
+# input path to segmentedAudio folder here and create the paddedAudio folder and put its path here as well.
+source_dir = 'path/to/segmentedAudio/folder'
+target_dir = 'path/to/paddedAudio/folder'
+
+os.makedirs(target_dir, exist_ok=True)
+
+wav_files = glob.glob(os.path.join(source_dir, '*.wav'))
+
+for wav_file in wav_files:
+    audio = AudioSegment.from_wav(wav_file)
+
+    # duration of silence in ms
+    silence = AudioSegment.silent(duration=300) # This is length of silence to pad beginning and end of segments. You can change this to whatever you want.
+    new_audio = audio + silence
+    new_file_path = os.path.join(target_dir, os.path.basename(wav_file))
+    new_audio.export(new_file_path, format='wav')
+
+print("Processing complete. Modified files are saved in:", target_dir)
