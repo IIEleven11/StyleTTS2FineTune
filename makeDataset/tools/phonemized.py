316afbe0085f7603b7471ccdbb83a00bd851ce9d
IIEleven11 <jakemottola6@gmail.com>
2024-06-02 22:18:09 -0700

    Update phonemized.py
    Added "with_stress" and "preserve_punctuation"

--- makeDataset/tools/phonemized.py
+++ makeDataset/tools/phonemized.py
@@ -2,15 +2,22 @@ import argparse
 from phonemizer import phonemize
 import os
 from tqdm import tqdm
+# from phonemizer.backend.espeak.wrapper import EspeakWrapper                ## For Windows
+# EspeakWrapper.set_library("C:\Program Files\eSpeak NG\libespeak-ng.dll")  ## For Windows
 
-# Create an argument parser
-parser = argparse.ArgumentParser(description='Phonemize transcriptions.')
-parser.add_argument('--language', type=str, default='en-us',
-                    help='The language to use for phonemization.')
+
+# argument parser
+parser = argparse.ArgumentParser(description="Phonemize transcriptions.")
+parser.add_argument(
+    "--language",
+    type=str,
+    default="en-us",
+    help="The language to use for phonemization.",
+)
 
 args = parser.parse_args()
 
-with open('./trainingdata/output.txt', 'r') as f: # Path to output.txt
+with open("./trainingdata/output.txt", "r") as f:  # Path to output.txt
     lines = f.readlines()
 
 # Phonemize the transcriptions
@@ -19,28 +26,43 @@ filenames = []
 transcriptions = []
 speakers = []
 phonemized_lines = []
-for line in lines:  # Split filenames, text and speaker without phonemizing. Prevents memory protection error.
-    filename, transcription, speaker = line.strip().split('|')
+for (
+    line
+) in lines:  # Split filenames, text and speaker without phonemizing. Prevents mem error
+    filename, transcription, speaker = line.strip().split("|")
     filenames.append(filename)
     transcriptions.append(transcription)
     speakers.append(speaker)
 
-phonemized = phonemize(transcriptions, language=args.language, backend='espeak')  # Phonemize all text in one go to avoid triggering the memory protections error
+# Phonemize all text in one go to avoid triggering  mem error
+phonemized = phonemize(
+    transcriptions,
+    language=args.language,
+    backend="espeak",
+    preserve_punctuation=True,
+    with_stress=True,
+)
 
-for i in tqdm(range(len(filenames))):  # Build the expected train_list
-    phonemized_lines.append((filenames[i], f'{filenames[i]}|{phonemized[i]}|{speakers[i]}\n'))
+for i in tqdm(range(len(filenames))):
+    phonemized_lines.append(
+        (filenames[i], f"{filenames[i]}|{phonemized[i]}|{speakers[i]}\n")
+    )
 
-# Sort
-phonemized_lines.sort(key=lambda x: int(x[0].split('_')[1].split('.')[0]))
 
-# Split training set and validation set
-train_lines = phonemized_lines[:int(len(phonemized_lines) * 0.9)]
-val_lines = phonemized_lines[int(len(phonemized_lines) * 0.9):]
+phonemized_lines.sort(key=lambda x: int(x[0].split("_")[1].split(".")[0]))
 
-with open('./trainingdata/train_list.txt', 'w+') as f: # Path for train_list.txt in the training data folder
+# Split training/validation set
+train_lines = phonemized_lines[: int(len(phonemized_lines) * 0.9)]
+val_lines = phonemized_lines[int(len(phonemized_lines) * 0.9) :]
+
+with open(
+    "./trainingdata/train_list.txt", "w+", encoding="utf-8"
+) as f:  # Path for train_list.txt in the training data folder
     for _, line in train_lines:
         f.write(line)
 
-with open('./trainingdata/val_list.txt', 'w+') as f:  # Path for val_list.txt in the training data folder
+with open(
+    "./trainingdata/val_list.txt", "w+", encoding="utf-8"
+) as f:  # Path for val_list.txt in the training data folder
     for _, line in val_lines:
         f.write(line)
